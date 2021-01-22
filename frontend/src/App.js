32af179e6b675f96e045a53bbfc835173ba5efe9
SharkBSJ <baesungjin1996@gmail.com>
2021-01-22 23:34:47 +0900

    Navbar Modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-
 import './App.css';
+import {useState} from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // Screens
@@ -9,14 +9,19 @@ import CartScreen from './screens/CartScreen'
 
 // Components
 import Navbar from './components/Navbar'
+import Backdrop from './components/Backdrop'
+import SideDrawer from './components/SideDrawer'
 
 function App() {
+
+  const [sideToggle, setSideToggle] = useState(false);
+
   return (
     <Router>
       <div className="app">
-        <Navbar />
-        {/* SideDrawler */}
-        {/* Backdrop */}
+        <Navbar click={() => setSideToggle(true)}/>
+        <SideDrawer show={sideToggle} click={() => setSideToggle(false)}/>
+        <Backdrop show={sideToggle} click={() => setSideToggle(false)}/>
         <main>
           <Switch>
             <Route exact path="/" component={HomeScreen}/>
@@ -24,9 +29,6 @@ function App() {
             <Route exact path="/cart" component={CartScreen}/>
           </Switch>
         </main>
-        {/* HomeScreen */}
-        {/* ProductScreen */}
-        {/* CartScreen */}
       </div>
     </Router>
   );
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,36 +2,31 @@ import './Navbar.css';
 import {Link} from 'react-router-dom';
 
 
-const Navbar = () => {
+const Navbar = ({click}) => {
   return (
-    <div className="navbar">
-        {/* logo */}
-        <div className="navbar_logo">
+    <nav className="navbar">
+        <div className="navbar__logo">
             <h2>MERN Shopping Cart</h2>
         </div>
-        {/* links */}
-        <div className="navbar_links">
+        <ul className="navbar__links">
             <li>
-                <Link to="/cart">
-                  { /* Icon */}
+                <Link to="/cart" className="cart__link">
                   <i className="fas fa-shopping-cart"></i>
-                  Cart
-                  <span className="cartlogo_badge">0</span>
+                  <span>
+                    Cart <span className="cartlogo__badge">0</span>
+                  </span>
                 </Link>
             </li>
             <li>
-                <Link to="/">
-                  Shop
-                </Link>
+                <Link to="/">Shop</Link>
             </li>
-        </div>
-        {/* hamburger menu */}
-        <div className="hamburger_menu">
+        </ul>
+        <div className="hamburger__menu" onClick={click}>
           <div></div>
           <div></div>
           <div></div>
         </div>
-    </div>
+    </nav>
   );
 }
 
