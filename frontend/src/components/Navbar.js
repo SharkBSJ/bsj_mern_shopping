ccaa1fdd6501736a2ba1d4a4dfa5812326ab563d
SharkBSJ <baesungjin1996@gmail.com>
2021-01-24 23:35:45 +0900

    Frontend Complete (Redux)

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,15 @@
 import './Navbar.css';
 import {Link} from 'react-router-dom';
-
+import {useSelector} from 'react-redux';
 
 const Navbar = ({click}) => {
+
+  const cart = useSelector(state => state.cart);
+  const {cartItems} = cart;
+  const getCartCount = () => {
+    return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
+  }
+
   return (
     <nav className="navbar">
         <div className="navbar__logo">
@@ -13,7 +20,7 @@ const Navbar = ({click}) => {
                 <Link to="/cart" className="cart__link">
                   <i className="fas fa-shopping-cart"></i>
                   <span>
-                    Cart <span className="cartlogo__badge">0</span>
+                    Cart <span className="cartlogo__badge">{getCartCount()}</span>
                   </span>
                 </Link>
             </li>
--- frontend/src/components/SideDrawer.js
+++ frontend/src/components/SideDrawer.js
@@ -1,11 +1,18 @@
 import './SideDrawer.css';
 import {Link} from 'react-router-dom'
+import {useSelector} from  'react-redux';
 
 const SideDrawer = ({show, click}) => {
     const sideDrawerClass = ["sidedrawer"];
 
     if (show) {
         sideDrawerClass.push("show");
+    }
+
+    const cart = useSelector(state => state.cart);
+    const {cartItems} = cart;
+    const getCartCount = () => {
+        return cartItems.reduce((qty, item) => Number(item.qty) + qty, 0);
     }
 
     return (
@@ -15,7 +22,7 @@ const SideDrawer = ({show, click}) => {
                 <Link to="/cart">
                     <i className="fas fa-shopping-cart"></i>
                     <span>
-                        Cart <span className="sidedrawer__cartbadge">0</span>
+                        Cart <span className="sidedrawer__cartbadge">{getCartCount()}</span>
                     </span>
                 </Link>
             </li>
